82490fd3e32509fe84dd26bf7080814c0fd93fc8
artem-mutaf <123882882+artem-mutaf@users.noreply.github.com>
2024-02-01 11:10:10 +0200

    Add files via upload

---
+++ tema16prkt3/DateClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tema16prkt3
+{
+    class DateClass
+    {
+        DateTime data;
+
+        public DateClass()
+        {
+
+        }
+        public DateClass(int year,int month,int day)
+        {
+            data = new DateTime(year, month, day);
+        }
+        public DateTime Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        public DateTime NextDay()
+        {
+            return data.AddDays(1);
+        }
+        public DateTime BackDay()
+        {
+            return data.AddDays(-1);
+        }
+        public int DaysToEnd()
+        {
+            return DateTime.DaysInMonth(data.Year, data.Month) - data.Day;
+        }
+    }
+}
---
+++ tema16prkt3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tema16prkt3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateClass date = new DateClass(2024,01,27);
+            
+            //DateTime date2 = DateTime.Now;
+            Console.WriteLine($"{date.Data.ToShortDateString()}");
+            Console.WriteLine($"Предыдущий день : {date.BackDay().ToShortDateString()}");
+            Console.WriteLine($"Следующий день : {date.NextDay().ToShortDateString()}");
+            Console.WriteLine($"Дней до конца месяца : {date.DaysToEnd()}");
+
+            DateTime date1 = DateTime.Parse(Console.ReadLine());
+            DateTime date2 = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine($"Разница между датами в {date1.Subtract(date2).ToString("dd")} дней");
+
+            
+            Console.ReadKey();
+        }
+    }
+}
